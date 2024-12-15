1da7e84a68d5ead52efffc3827490ef8762ccc23
MukeshPullabhatla <amukeshps@gmail.com>
2024-12-15 17:17:47 -0500

    My First Commit

---
+++ LeetCode-CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode_CSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // TwoSum
+            TwoSumSolution solution = new TwoSumSolution();
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result = solution.TwoSum(nums, target);
+            Console.WriteLine("[" + result[0] + ", " + result[1] + "]");
+        }
+    }
+}
---
+++ LeetCode-CSharp/TwoSum.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode_CSharp
+{
+    public class TwoSumSolution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numDict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int diff = target - nums[i];
+                if (numDict.ContainsKey(diff))
+                {
+                    return new int[] { numDict[diff], i };
+                }
+                if (!numDict.ContainsKey(nums[i]))
+                    numDict.Add(nums[i], i);
+            }
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+}
