8c1acbda4ca9fba004a751a5864daada03448f9d
MukeshPullabhatla <amukeshps@gmail.com>
2024-12-15 21:25:02 -0500

    Add Two Numbers

--- LeetCode-CSharp/Program.cs
+++ LeetCode-CSharp/Program.cs
@@ -1,15 +1,37 @@
-﻿namespace LeetCode_CSharp
+﻿using static LeetCode_CSharp.AddTwoNumbersSolution;
+
+namespace LeetCode_CSharp
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            // TwoSum
+            // Two Sum
             TwoSumSolution solution = new TwoSumSolution();
             int[] nums = { 2, 7, 11, 15 };
             int target = 9;
             int[] result = solution.TwoSum(nums, target);
             Console.WriteLine("[" + result[0] + ", " + result[1] + "]");
+
+            // Add Two Numbers
+            ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+            ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+            AddTwoNumbersSolution aTNSolution = new AddTwoNumbersSolution();
+            ListNode res = aTNSolution.AddTwoNumbers(l1, l2);
+
+            if (res != null)
+            {
+                while (res != null)
+                {
+                    Console.Write(res.val);
+                    res = res.next;
+                    if (res != null) Console.Write(" -> ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Result is null");
+            }
         }
     }
 }
--- LeetCode-CSharp/obj/Debug/net8.0/LeetCode-CSharp.AssemblyInfo.cs
+++ LeetCode-CSharp/obj/Debug/net8.0/LeetCode-CSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCode-CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da7e84a68d5ead52efffc3827490ef8762ccc23")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCode-CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCode-CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
